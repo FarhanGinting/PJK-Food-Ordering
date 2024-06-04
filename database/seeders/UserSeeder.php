127d42500fd74d02941f5dc14589e11d6444285f
Farhan <118242884+FarhanGinting@users.noreply.github.com>
2024-06-04 09:45:03 +0700

    ✅ Fitur Login & Middleware

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Schema::disableForeignKeyConstraints();
+        // User::truncate();
+        // Schema::enableForeignKeyConstraints();
+
+        // User::insert([
+        //     'name' => 'manager',
+        //     'email' => 'manager@email.com',
+        //     'password' => Hash::make('123'),
+        //     'role_id' => 4,
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
+    }
+}
